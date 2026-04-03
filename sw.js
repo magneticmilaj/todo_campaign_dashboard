1d73ab9fedec0b5c8f3b94fc3301c95ec8f01eea
magneticmilaj <minjeongy1122@gmail.com>
2026-04-03 12:32:45 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'tododoc-v1';
+const ASSETS = [
+  '/todo_campaign_dashboard/',
+  '/todo_campaign_dashboard/index.html',
+  '/todo_campaign_dashboard/manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
